995146c802b6e53175d0dea1d5b6d63d7ff147ce
Dannika Covarrubias <S-dannika.covarrubia@lwtech.edu>
2025-12-08 14:37:47 -0800

    Apply Black to calculator.py

--- calculator.py
+++ calculator.py
@@ -9,14 +9,14 @@ while 1:
     choice = int(input("Enter your choice: "))
     num1 = int(input("Enter first number: "))
     num2 = int(input("Enter second number: "))
-    if choice == 1:    
-        print("Output: ", num1+num2)
+    if choice == 1:
+        print("Output: ", num1 + num2)
     elif choice == 2:
-        print("Output: ", num1-num2)
+        print("Output: ", num1 - num2)
     elif choice == 3:
-        print("Output: ", num1*num2)
+        print("Output: ", num1 * num2)
     elif choice == 4:
-        print("Output: ", num1/num2)
+        print("Output: ", num1 / num2)
     elif choice == 5:
         print("Exiting...")
         break
