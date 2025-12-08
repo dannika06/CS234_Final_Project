d2fd80dcd2d62f63039e88e1299ace1ae44ef155
Dannika Covarrubias <S-dannika.covarrubia@lwtech.edu>
2025-12-08 14:41:30 -0800

    Apply black to password_strength_checker.py

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -1,8 +1,11 @@
 import string
 import getpass
 
+
 def check_password_strength(password):
-    lower_alpha_count = upper_alpha_count = number_count = whitespace_count = special_char_count = 0
+    lower_alpha_count = upper_alpha_count = number_count = whitespace_count = (
+        special_char_count
+    ) = 0
     for char in list(password):
         if char in string.ascii_lowercase:
             lower_alpha_count += 1
@@ -10,12 +13,12 @@ def check_password_strength(password):
             upper_alpha_count += 1
         elif char in string.digits:
             number_count += 1
-        elif char == ' ':
+        elif char == " ":
             whitespace_count += 1
         else:
             special_char_count += 1
     strength = 0
-    remarks = ''
+    remarks = ""
 
     if lower_alpha_count >= 1:
         strength += 1
@@ -31,7 +34,9 @@ def check_password_strength(password):
     if strength == 1:
         remarks = "That's a very bad password. Change it as soon as possible."
     elif strength == 2:
-        remarks = "That's not a good password. You should consider making a tougher password."
+        remarks = (
+            "That's not a good password. You should consider making a tougher password."
+        )
     elif strength == 3:
         remarks = "Your password is okay, but it can be improved a lot"
     elif strength == 4:
@@ -43,20 +48,21 @@ def check_password_strength(password):
     print(f"{lower_alpha_count} lowercase letters")
     print(f"{upper_alpha_count} uppercase letters")
     print(f"{number_count} digits")
-    print(f'{whitespace_count} whitespaces')
+    print(f"{whitespace_count} whitespaces")
     print(f"{special_char_count} special characters")
     print(f"Password score: {strength}/5")
     print(f"Remarks: {remarks}")
 
+
 print("===== Welcome to Password Strength Checker =====")
 while 1:
     choice = input("Do you want to check a password's strength (y/n) : ")
-    if 'y' in choice.lower():
+    if "y" in choice.lower():
         password = getpass.getpass("Enter the password: ")
         check_password_strength(password)
-    elif 'n' in choice.lower():
-        print('Exiting...')
+    elif "n" in choice.lower():
+        print("Exiting...")
         break
     else:
-        print('Invalid input...please try again.')
+        print("Invalid input...please try again.")
     print()
