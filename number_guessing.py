937dd7b6e0741fbe5820fb3aa816b64c43e6aefc
Dannika Covarrubias <S-dannika.covarrubia@lwtech.edu>
2025-12-08 14:40:49 -0800

    Apply Black to number_guessing.py

--- number_guessing.py
+++ number_guessing.py
@@ -1,14 +1,16 @@
 import random
 
+
 def get_random_number():
     random_number = random.randint(0, 9)
     return random_number
+
 
 print("===== Welcome to Number Guessing Game =====")
 chances = 0
 while 1:
     choice = input("Do you wanna play (y/n): ")
-    if 'y' in choice.lower():
+    if "y" in choice.lower():
         print("Generating random number between 0 and 9")
         random_number = get_random_number()
         while 1:
@@ -18,7 +20,11 @@ while 1:
                 print("Invalid input...please try again")
             else:
                 if user_number == random_number:
-                    print("Conguratulation !!! You got the number in {0} chances\n".format(chances))
+                    print(
+                        "Conguratulation !!! You got the number in {0} chances\n".format(
+                            chances
+                        )
+                    )
                     chances = 0
                     break
                 elif user_number > random_number:
@@ -31,7 +37,7 @@ while 1:
                         print("Close but not cigar, try a higher number")
                     else:
                         print("Not even close, try a higher number")
-    elif 'n' in choice.lower():
+    elif "n" in choice.lower():
         print("Exiting...")
         break
     else:
