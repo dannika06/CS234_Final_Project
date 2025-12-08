234d2fb29ea02c3225015e193a7d4fe33be37d43
Dannika Covarrubias <S-dannika.covarrubia@lwtech.edu>
2025-12-08 14:43:59 -0800

    Apply black to value_converter.py

--- value_converter.py
+++ value_converter.py
@@ -12,6 +12,7 @@ def convert_temperature():
     else:
         print("Invalid input...please try again\n")
 
+
 def convert_currency():
     print("\nWhich conversion do you want to choose:-")
     print("1. Dollar to pound")
@@ -24,6 +25,7 @@ def convert_currency():
         value = float(input("Enter currency in pounds: "))
         print(f"{value} pounds in dollars will be {value/0.73}\n")
 
+
 def convert_lengths():
     print("\nWhich conversion do you want to choose:-")
     print("1. Centimeters to foot and inches")
@@ -31,14 +33,15 @@ def convert_lengths():
     choice = int(input("Enter your choice: "))
     if choice == 1:
         value = float(input("Enter length in cm: "))
-        inches = value/2.54
-        feet = inches/12
+        inches = value / 2.54
+        feet = inches / 12
         print(f"{value} centimeters in equal to {feet} feet and {inches} inch\n")
     elif choice == 2:
         feet = float(input("Enter length in feet: "))
         inches = float(input("Enter length in inches: "))
-        length = (feet*12 + inches)*2.54
+        length = (feet * 12 + inches) * 2.54
         print(f"{feet} feet and {inches} inches in centimeters will be {length}\n")
+
 
 print("===== Welcome to Value Converter =====")
 while 1:
@@ -55,5 +58,5 @@ while 1:
     elif choice == 3:
         convert_lengths()
     elif choice == 4:
-        print('Exiting...')
+        print("Exiting...")
         exit(0)
